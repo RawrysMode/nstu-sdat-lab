ebb29e02c7fdd545b220359cf8cf2cc8130b1b06
RawrysMode <71321626+RawrysMode@users.noreply.github.com>
2022-11-13 01:41:43 +0700

    Update Serialization.java
    Major changes for loadToFile and readFromFile methods

--- src/Serialization.java
+++ src/Serialization.java
@@ -1,26 +1,39 @@
+import factory.UserFactory;
+import factory.UserType;
+
 import java.io.*;
+import java.util.ArrayList;
 
 public class Serialization {
+    static String returnedValue;
 
-    public static void loadToFile(Heap heap){
-        try {
-            FileOutputStream fos = new FileOutputStream("temp.bin");
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-
-            oos.writeObject(heap);
-            oos.flush();
-            oos.close();
+    public static String loadToFile(Heap heap){
+        String type = Heap.getObject().typeName();
+        try (PrintWriter writer = new PrintWriter("saved_" + type + ".txt")){
+            writer.print(type + "\n");
+            heap.getHeapArray().forEach(e -> writer.print(e + ","));
+            return " > successfully uploaded to file\n";
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            return " > failed to upload to file: " + e + "\n";
         }
     }
 
-    public static Heap readFromFile(String filename){
-        try {
-            FileInputStream fis = new FileInputStream("temp.bin");
-            ObjectInputStream oin = new ObjectInputStream(fis);
-            return (Heap) oin.readObject();
-        } catch (IOException | ClassNotFoundException e) {
+    public static Heap readFromFile(String filename) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String typeName = br.readLine();
+            String lineOfItems = br.readLine();
+            String[] items = lineOfItems.split(",");
+
+            ArrayList<UserType> arrayList = new ArrayList<>();
+            for (String item : items) {
+                UserType object = UserFactory.getBuilderByName(typeName);
+                object.parseValue(item);
+                arrayList.add(object);
+            }
+            returnedValue = " > data uploaded successfully\n";
+            return new Heap(typeName, arrayList);
+        } catch (Exception e) {
+            returnedValue = " > failed to load data from file file\n" + e;
             throw new RuntimeException(e);
         }
     }
