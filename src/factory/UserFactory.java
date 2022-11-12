044f9a181292f3df3796c012ea3a7a0509c98a4c
RawrysMode <71321626+RawrysMode@users.noreply.github.com>
2022-11-13 01:35:17 +0700

    Update UserFactory.java
    Changed getBuilderByName method

--- src/factory/UserFactory.java
+++ src/factory/UserFactory.java
@@ -2,17 +2,24 @@ package factory;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Supplier;
 
 public class UserFactory {
-    public static ArrayList<String> getTypeNameList(){
+    public static ArrayList<String> getTypeNameList() {
         return new ArrayList<>(Arrays.asList("integer", "datetime"));
     }
 
-    public static UserTypeFactory getBuilderByName(String name){
-        return switch (name.toLowerCase()) {
-            case "integer" -> new IntegerFactory();
-            case "datetime" -> new DatetimeFactory();
-            default -> null;
-        };
+    private static final Map<String, Supplier<UserType>> factoryMap = Map.of(
+            "integer", IntegerType::new,
+            "datetime", DatetimeType::new
+    );
+
+    public static UserType getBuilderByName(String name) {
+        Supplier<UserType> factory = factoryMap.get(name);
+        if (factory == null) {
+            throw new RuntimeException();
+        }
+        return factory.get();
     }
 }
