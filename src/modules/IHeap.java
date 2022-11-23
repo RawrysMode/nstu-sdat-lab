0676559816399de2b618adcea1d55cb246e9b0e3
RawrysMode <71321626+RawrysMode@users.noreply.github.com>
2022-11-24 00:18:32 +0700

    Create IHeap.java

---
+++ src/modules/IHeap.java
@@ -0,0 +1,20 @@
+package modules;
+
+import factory.UserType;
+
+import java.util.List;
+
+public interface IHeap {
+    StringBuilder printHeap();
+    StringBuilder insertNode(UserType userType);
+    StringBuilder insertNode(int index, UserType userType);
+    StringBuilder removeNode(int index);
+    void displaceUp(int index);
+    void displaceDown(int n, int index);
+    StringBuilder sort();
+    StringBuilder sortToMaxHeap();
+    StringBuilder getElementByIndex(int index);
+    StringBuilder printArray();
+    List<UserType> getHeapArray();
+    void setCurrentSizeToZero();
+}
