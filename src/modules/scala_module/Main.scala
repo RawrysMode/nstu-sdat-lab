b14831e6b8790c3ce1664aef3023b007b270fc93
RawrysMode <71321626+RawrysMode@users.noreply.github.com>
2022-11-24 00:21:39 +0700

    Create Main.scala

---
+++ src/modules/scala_module/Main.scala
@@ -0,0 +1,41 @@
+package modules.scala_module
+
+import factory.{UserFactory, UserType}
+import modules.IHeap
+
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+
+object Main extends App {
+  //new HeapManagerGUI;
+
+  testIntegerType(1000000)
+  testIntegerType(1200000)
+  testIntegerType(1400000)
+  testIntegerType(1600000)
+  testIntegerType(1800000)
+  testIntegerType(2000000)
+  testIntegerType(2200000)
+  testIntegerType(2400000)
+  testIntegerType(2600000)
+  testIntegerType(2800000)
+  testIntegerType(3000000)
+
+  def testIntegerType(numberOfRequiredNodes: Int): Unit = {
+    val rangeBegin = -99999
+    val rangeEnd = 99999
+    val difference = rangeEnd - rangeBegin
+    val arrayList = new ArrayBuffer[UserType]
+
+    for (_ <- 0 until numberOfRequiredNodes) {
+      val value: UserType = UserFactory.getBuilderByName("integer")
+      value.parseValue((Random.nextInt(difference) + rangeBegin).toString)
+      arrayList += value
+    }
+
+    val heap: IHeap = new Heap(arrayList.toArray)
+    print(heap.sort)
+  }
+}
+
+
